c9d33532d2a2de0f0c5b61732adcab4c5f8e3f33
Markos <markosmk@me.com>
2021-11-22 20:27:07 -0300

    [add] globalstyles

---
+++ styles/global.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    marginTop: 20,
+    padding: 20,
+  },
+  titulo: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 30,
+    fontSize: 30,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 20,
+    right: 0,
+    bottom: 20,
+  },
+});
+
+export default globalStyles;
