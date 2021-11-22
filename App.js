adcc1c7eae94c2b048a512bbc4eabc69691aed30
Markos <markosmk@me.com>
2021-11-22 20:27:49 -0300

    [update] add Theme Provider

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+// Componentes
+import Inicio from './views/Inicio';
+import NuevoCliente from './views/NuevoCliente';
+import DetallesCliente from './views/DetallesCliente';
+// interfaz
+import Header from './components/Header';
+
+const Stack = createStackNavigator();
+
+// Definiendo el Tema
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2', // sobreescribimos el color principal
+    accent: '#0655BF',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Inicio"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: theme.colors.primary,
+              },
+              headerTintColor: theme.colors.surface,
+              headerTitleAlign: 'center',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          >
+            <Stack.Screen
+              name="Inicio"
+              component={Inicio}
+              options={({ navigation, route }) => ({
+                /*headerLeft: (props) => (
+                  <Header {...props} navigation={navigation} route={route} />
+                ),*/
+              })}
+            />
+            <Stack.Screen
+              name="NuevoCliente"
+              component={NuevoCliente}
+              options={{
+                title: 'Nuevo Cliente',
+              }}
+            />
+            <Stack.Screen
+              name="DetallesCliente"
+              component={DetallesCliente}
+              options={{
+                title: 'Detalles Cliente',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
